70b14ac1256f254e4ee93f5d86a83d97a85f47b2
Sergiu994 <sergiu_coicheci@yahoo.com>
2016-10-19 23:53:09 +0300

    Tema1
    Update

---
+++ PSSC/Models/source/IStudent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    interface IStudent
+    {
+        string Nume_student
+        {
+            get;
+            set;
+        }
+        string Nr_matricol
+        {
+            get;
+            set;
+        }
+        string CNP
+        {
+            get;
+            set;
+        }
+        int An_studiu
+        {
+            get;
+            set;
+        }
+    }
+}
